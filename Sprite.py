7ffab82054fcdc34f28630f3acca9e1a9ccc816b
Doris Leong <dleong@csu.fullerton.edu>
2016-12-04 19:27:51 -0800

    Add Instructions, difficulty

--- Sprite.py
+++ Sprite.py
@@ -14,8 +14,10 @@ from pygame.locals import *
 
 # Define some colors
 BLACK = (0, 0, 0)
+GRAY = (100, 100, 100)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+PURPLE = (255, 0, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
@@ -31,7 +33,7 @@ class Block(pygame.sprite.Sprite):
         # Call the parent class (Sprite) constructor
         super().__init__()
 
-        self.image = pygame.Surface([20, 15])
+        self.image = pygame.Surface([15, 15])
         self.image.fill(color)
 
         self.rect = self.image.get_rect()
@@ -71,13 +73,15 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
         elif self.rect.right > 800:
             self.rect.right = 800
-        if self.rect.top <= 20:
-            self.rect.top = 20
+        if self.rect.top <= 30:
+            self.rect.top = 30
         elif self.rect.bottom >= 600:
             self.rect.bottom = 600
 
         self.rect.x += self.change_x
         self.rect.y += self.change_y
+
+# referenced code: https://gist.github.com/iminurnamez/9abb72c3e43309cb6098
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -208,17 +212,17 @@ enemy_list = pygame.sprite.Group()
 
 # --- Create the sprites
 
-for i in range(50):
-    # This represents a block
-    block = Block(GREEN)
+# for i in range(10):
+#     # This represents a block
+#     block = Block(D_RED)
 
-    # Set a random location for the block
-    block.rect.x = random.randrange(SCREEN_WIDTH)
-    block.rect.y = random.randrange(SCREEN_HEIGHT - 50)
+#     # Set a random location for the block
+#     block.rect.x = random.randrange(SCREEN_WIDTH)
+#     block.rect.y = random.randrange(30, SCREEN_HEIGHT - 50)
 
-    # Add the block to the list of objects
-    block_list.add(block)
-    all_sprites_list.add(block)
+#     # Add the block to the list of objects
+#     block_list.add(block)
+#     all_sprites_list.add(block)
 
 # Create a red player block
 player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -236,19 +240,36 @@ done = False
 clock = pygame.time.Clock()
 
 score = 0
+lives = 3
+
+spawn_timer = 1000
+max_followers = 5
+max_blocks = 20
 
 player.rect.x = SCREEN_WIDTH / 2
 player.rect.y = SCREEN_HEIGHT / 2
 
 SHOOT_EVENT = pygame.USEREVENT + 1
 ENEMY_SPAWN_EVENT = pygame.USEREVENT + 2
-pygame.time.set_timer(pygame.USEREVENT + 2, 1000)
+STUCK_EVENT = pygame.USEREVENT + 3
+pygame.time.set_timer(pygame.USEREVENT + 2, spawn_timer)
 x, y = 0, 0
 
 moveLeft = False
 moveRight = False
 moveUp = False
 moveDown = False
+game_over = False
+stuck = False
+font = pygame.font.Font(None, 20)
+large_font = pygame.font.Font(None, 48)
+display_instructions = True
+instruction_page_image = pygame.image.load("Instructions.png").convert()
+instruction_page = 1
+level_2_reached = False
+level_3_reached = False
+level_4_reached = False
+level_5_reached = False
 
 
 def shoot(pos):
@@ -264,6 +285,44 @@ def shoot(pos):
     all_sprites_list.add(bullet)
     bullet_list.add(bullet)
 
+while not done and display_instructions:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            instruction_page += 1
+            if instruction_page == 2:
+                display_instructions = False
+
+    # Set the screen background
+    screen.blit(instruction_page_image, [0, 0])
+
+    if instruction_page == 1:
+        # Draw instructions, page 1
+        # This could also load an image created in another program.
+        # That could be both easier and more flexible.
+
+        text = font.render("Instructions", True, WHITE)
+        screen.blit(text, [10, 10])
+
+        text = font.render("Page 1", True, WHITE)
+        screen.blit(text, [10, 40])
+
+    if instruction_page == 2:
+        # Draw instructions, page 2
+        text = font.render("This program bounces a rectangle", True, WHITE)
+        screen.blit(text, [10, 10])
+
+        text = font.render("Page 2", True, WHITE)
+        screen.blit(text, [10, 40])
+
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Event Processing
@@ -271,6 +330,30 @@ while not done:
         key = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             done = True
+
+        # Restart game with space if game over
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                if game_over:
+                    game_over = False
+                    score = 0
+                    lives = 3
+                    spawn_timer = 1000
+                    max_followers = 5
+                    max_blocks = 10
+                    level_2_reached = False
+                    level_3_reached = False
+                    level_4_reached = False
+                    level_5_reached = False
+                    pygame.time.set_timer(pygame.USEREVENT + 2, spawn_timer)
+                    enemy_list.empty()
+                    block_list.empty()
+                    bullet_list.empty()
+                    all_sprites_list.empty()
+                    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+                    all_sprites_list.add(player)
+                    player.rect.x = SCREEN_WIDTH / 2
+                    player.rect.y = SCREEN_HEIGHT / 2
 
         # Fire a bullet if the user clicks the mouse button
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -282,19 +365,38 @@ while not done:
             m_pos = pygame.mouse.get_pos()
             shoot(m_pos)
 
+        if event.type == STUCK_EVENT:
+            pygame.time.set_timer(pygame.USEREVENT + 3, 0)
+            stuck = False
+
         if event.type == ENEMY_SPAWN_EVENT:
-            # TODO spawn outside certain range of player
-            if len(enemy_list) < 5:
+            # Spawn enemies that follow
+            if len(enemy_list) < max_followers:
                 close = True
                 while close:
                     random_pos = (random.randrange(SCREEN_WIDTH),
-                                  random.randrange(20, SCREEN_HEIGHT - 50))
+                                  random.randrange(30, SCREEN_HEIGHT - 50))
                     x_distance_to_player = abs(random_pos[0] - player.rect.x)
                     y_distance_to_player = abs(random_pos[1] - player.rect.y)
                     if x_distance_to_player > 100 and y_distance_to_player > 100:
                         close = False
                 enemy = Enemy(random_pos, player.getPlayerPos())
                 enemy_list.add(enemy)
+            # Spawn stationary enemies
+            if len(block_list) < max_blocks:
+                close = True
+                while close:
+                    random_pos = (random.randrange(SCREEN_WIDTH),
+                                  random.randrange(30, SCREEN_HEIGHT - 50))
+                    x_distance_to_player = abs(random_pos[0] - player.rect.x)
+                    y_distance_to_player = abs(random_pos[1] - player.rect.y)
+                    if x_distance_to_player > 50 and y_distance_to_player > 50:
+                        close = False
+                block = Block(PURPLE)
+                block.rect.x = random_pos[0]
+                block.rect.y = random_pos[1]
+                block_list.add(block)
+                all_sprites_list.add(block)
 
         if event.type == pygame.MOUSEBUTTONUP:
             pygame.time.set_timer(pygame.USEREVENT + 1, 0)
@@ -318,67 +420,122 @@ while not done:
                 moveUp = False
             elif event.key in (K_DOWN, K_s):
                 moveDown = False
-
-        if moveUp and moveLeft:
-            player.changespeed(-2, -2)
-        elif moveUp and moveRight:
-            player.changespeed(2, -2)
-        elif moveDown and moveLeft:
-            player.changespeed(-2, 2)
-        elif moveDown and moveRight:
-            player.changespeed(2, 2)
-        elif moveUp:
-            player.changespeed(0, -3)
-        elif moveDown:
-            player.changespeed(0, 3)
-        elif moveLeft:
-            player.changespeed(-3, 0)
-        elif moveRight:
-            player.changespeed(3, 0)
-        else:
+        if stuck:
             player.changespeed(0, 0)
+        else:
+            if moveUp and moveLeft:
+                player.changespeed(-2, -2)
+            elif moveUp and moveRight:
+                player.changespeed(2, -2)
+            elif moveDown and moveLeft:
+                player.changespeed(-2, 2)
+            elif moveDown and moveRight:
+                player.changespeed(2, 2)
+            elif moveUp:
+                player.changespeed(0, -3)
+            elif moveDown:
+                player.changespeed(0, 3)
+            elif moveLeft:
+                player.changespeed(-3, 0)
+            elif moveRight:
+                player.changespeed(3, 0)
+            else:
+                player.changespeed(0, 0)
 
     # --- Game logic
+    if not game_over:
 
-    # Call the update() method on all the sprites
-    all_sprites_list.update()
-    enemy_list.update(player.getPlayerPos())
-    # Calculate mechanics for each bullet
-    for bullet in bullet_list:
+        # Call the update() method on all the sprites
+        all_sprites_list.update()
+        enemy_list.update(player.getPlayerPos())
+        # Calculate mechanics for each bullet
+        for bullet in bullet_list:
 
-        # See if it hit a block or enemy
-        block_hit_list = pygame.sprite.spritecollide(bullet, block_list, True)
-        enemy_hit_list = pygame.sprite.spritecollide(bullet, enemy_list, True)
+            # See if it hit a block or enemy
+            block_hit_list = pygame.sprite.spritecollide(
+                bullet, block_list, True)
+            enemy_hit_list = pygame.sprite.spritecollide(
+                bullet, enemy_list, True)
 
-        # For each block hit, remove the bullet and add to the score
-        for block in block_hit_list:
-            bullet_list.remove(bullet)
-            all_sprites_list.remove(bullet)
-            score += 1
-            print(score)
+            # For each block hit, remove the bullet and add to the score
+            for block in block_hit_list:
+                bullet_list.remove(bullet)
+                all_sprites_list.remove(bullet)
+                all_sprites_list.remove(block)
+                #score += 1
+                print("Stationary killed -> ", score)
 
-        for enemy in enemy_hit_list:
-            bullet_list.remove(bullet)
-            all_sprites_list.remove(bullet)
-            score += 10
-            print("Enemy killed -> ", score)
+            for enemy in enemy_hit_list:
+                bullet_list.remove(bullet)
+                all_sprites_list.remove(bullet)
+                score += 10
+                print("Follower killed -> ", score)
 
-        # Remove the bullet if it flies off the screen
-        if bullet.rect.y < -10 or bullet.rect.y > SCREEN_HEIGHT + 10 or bullet.rect.x < -10 or bullet.rect.x > SCREEN_WIDTH + 10:
-            bullet_list.remove(bullet)
-            all_sprites_list.remove(bullet)
+            # Remove the bullet if it flies off the screen
+            if bullet.rect.y < -10 or bullet.rect.y > SCREEN_HEIGHT + 10 or bullet.rect.x < -10 or bullet.rect.x > SCREEN_WIDTH + 10:
+                bullet_list.remove(bullet)
+                all_sprites_list.remove(bullet)
 
-    if pygame.sprite.spritecollideany(player, enemy_list) != None:
-        print("Player was hit!")
-        enemy_list.remove(pygame.sprite.spritecollideany(player, enemy_list))
+        if pygame.sprite.spritecollideany(player, enemy_list) != None:
+            pygame.time.wait(500)
+            print("Player was hit by follower!")
+            enemy_list.remove(
+                pygame.sprite.spritecollideany(player, enemy_list))
+            lives -= 1
+            if lives == 0:
+                print("GAME OVER")
+                # show game over screen
+                game_over = True
+                pygame.time.set_timer(pygame.USEREVENT + 2, 0)
+
+        if pygame.sprite.spritecollideany(player, block_list) != None:
+            print("Player hit a stationary enemy!")
+            hit_block = pygame.sprite.spritecollideany(player, block_list)
+            block_list.remove(hit_block)
+            all_sprites_list.remove(hit_block)
+            stuck = True
+            pygame.time.set_timer(pygame.USEREVENT + 3, 500)
+
+        if not level_2_reached and score >= 100:
+            print("---------LEVEL 2---------")
+            level_2_reached = True
+            pygame.time.set_timer(pygame.USEREVENT + 2, 750)
+        elif not level_3_reached and score >= 250:
+            print("---------LEVEL 3---------")
+            level_3_reached = True
+            pygame.time.set_timer(pygame.USEREVENT + 2, 500)
+        elif not level_4_reached and score >= 500:
+            print("---------LEVEL 4---------")
+            level_4_reached = True
+            pygame.time.set_timer(pygame.USEREVENT + 2, 300)
+            max_blocks = 40
+        elif not level_5_reached and score >= 1000:
+            print("---------LEVEL 5---------")
+            level_5_reached = True
+            pygame.time.set_timer(pygame.USEREVENT + 2, 200)
+
     # --- Draw a frame
 
     # Clear the screen
     screen.fill(WHITE)
 
+    # draw bar on top
+    pygame.draw.rect(screen, GRAY, [0, 0, 800, 30])
+    display_score = font.render("Score: " + str(score), True, BLACK)
+    screen.blit(display_score, [10, 10])
+
+    display_lives = font.render("Lives: x" + str(lives), True, BLACK)
+    screen.blit(display_lives, [725, 10])
+
     # Draw all the spites
     all_sprites_list.draw(screen)
     enemy_list.draw(screen)
+
+    if game_over:
+        display_game_over = large_font.render("GAME OVER", True, BLACK)
+        screen.blit(display_game_over, [300, 250])
+        restart = font.render("Press space to restart", True, BLACK)
+        screen.blit(restart, [330, 350])
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
